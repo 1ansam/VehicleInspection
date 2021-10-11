c19deb3251d31627f0b469b2e088447133525f90
1ansam <1229921114@qq.com>
2021-10-11 21:48:37 +0800

    添加API对应实体类

--- app/src/test/java/com/yxf/vehicleinspection/ExampleUnitTest.kt
+++ app/src/test/java/com/yxf/vehicleinspection/ExampleUnitTest.kt
@@ -1,18 +1,6 @@
 package com.yxf.vehicleinspection
 
-import com.yxf.vehicleinspection.base.BaseUrlHelper
-import com.yxf.vehicleinspection.bean.DataJson
-import com.yxf.vehicleinspection.service.DataService
-import com.yxf.vehicleinspection.singleton.RetrofitService
-import com.yxf.vehicleinspection.utils.IpHelper
-import com.yxf.vehicleinspection.utils.TableJsCsCodeHelper
-import okhttp3.HttpUrl
-import org.junit.Test
-import retrofit2.Callback
-import retrofit2.Retrofit
-import retrofit2.http.Url
-import java.net.URL
-import java.util.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
