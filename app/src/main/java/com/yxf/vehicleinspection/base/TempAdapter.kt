67535a2789573e2860484de03dde279f58caf3c9
1ansam <1229921114@qq.com>
2021-11-04 22:39:26 +0800

    commit 2021.11.04

---
+++ app/src/main/java/com/yxf/vehicleinspection/base/TempAdapter.kt
@@ -0,0 +1,23 @@
+package com.yxf.vehicleinspection.base
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.yxf.vehicleinspection.bean.response.VehicleInspectionItemResponse
+import com.yxf.vehicleinspection.databinding.InspectionInfoItemBinding
+import com.yxf.vehicleinspection.databinding.RvItemInspectionItemBinding
+
+/**
+ * @author : yxf
+ * @Date : 2021/11/4
+ **/
+abstract class TempAdapter<T : Any, V : ViewBinding>(t : T) : RecyclerView.Adapter<TempViewHolder<V>>(){
+    open var data : T = t
+    set(value) {
+        field = value
+        notifyDataSetChanged()
+    }
+
+
+}
+class TempViewHolder<V : ViewBinding>(val binding : V) : RecyclerView.ViewHolder(binding.root)
