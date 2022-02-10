b421f573b19a0fbb10e874be3c74a0fa5425f3d4
yxf <1anxuf@gmail.com>
2022-02-10 09:39:50 +0800

    优化

--- app/src/main/java/com/yxf/vehicleinspection/view/fragment/ExteriorFragment.kt
+++ app/src/main/java/com/yxf/vehicleinspection/view/fragment/ExteriorFragment.kt
@@ -467,54 +467,54 @@ class ExteriorFragment : BaseBindingFragment<FragmentExteriorBinding>() {
             }
         }
 
-        systemParamsViewModel.getJyjgbh("AJ").observe(this) {
-            AjJyjghb = it
-            systemParamsViewModel.getJyjgbh("HJ").observe(this) {
-                HjJyjghb = it
-                systemParamsViewModel.getWebPass("AJ").observe(this) {
-                    AjJkxlh = it
-                    inspectionItemViewModel.getServerTime().observe(this) {
-                        beginTime = it.Sj
-                        inspectionItemViewModel.postProjectStartW010(
-                            ProjectStartW010Request(
-                                AjJyjghb,
-                                args.jcxh,
-                                args.bean005.Hphm,
-                                args.bean005.Hpzl,
-                                args.bean005.Clsbdh,
-                                args.bean006.Jcxm,
-                                args.bean006.Jcxm,
-                                beginTime,
-                                args.bean006.Ajywlb,
-                                args.bean006.Hjywlb,
-                                AjJkxlh,
-                                args.bean002.Ajlsh,
-                                args.bean002.Hjlsh,
-                                args.bean002.Ajjccs,
-                                args.bean002.Hjjccs
-                            )
-                        ).observe(this) {
-                            if (it) {
-                                getImageData(
-                                    args.bean006.Jcxm,
-                                    args.bean006.Ajywlb,
-                                    args.bean006.Hjywlb,
-                                    args.bean002.Ajlsh,
-                                    args.bean002.Hjlsh
-                                )
-                                getSelectData(
-                                    args.bean006.Jcxm,
-                                    args.bean006.Ajywlb, args.bean006.Hjywlb,
-                                    args.bean002.Ajlsh, args.bean002.Hjlsh
-                                )
-                            } else {
-                                Toast.makeText(MyApp.context, "写入项目开始失败", Toast.LENGTH_SHORT).show()
-                            }
-                        }
-                    }
-                }
-            }
-        }
+//        systemParamsViewModel.getJyjgbh("AJ").observe(this) {
+//            AjJyjghb = it
+//            systemParamsViewModel.getJyjgbh("HJ").observe(this) {
+//                HjJyjghb = it
+//                systemParamsViewModel.getWebPass("AJ").observe(this) {
+//                    AjJkxlh = it
+//                    inspectionItemViewModel.getServerTime().observe(this) {
+//                        beginTime = it.Sj
+//                        inspectionItemViewModel.postProjectStartW010(
+//                            ProjectStartW010Request(
+//                                AjJyjghb,
+//                                args.jcxh,
+//                                args.bean005.Hphm,
+//                                args.bean005.Hpzl,
+//                                args.bean005.Clsbdh,
+//                                args.bean006.Jcxm,
+//                                args.bean006.Jcxm,
+//                                beginTime,
+//                                args.bean006.Ajywlb,
+//                                args.bean006.Hjywlb,
+//                                AjJkxlh,
+//                                args.bean002.Ajlsh,
+//                                args.bean002.Hjlsh,
+//                                args.bean002.Ajjccs,
+//                                args.bean002.Hjjccs
+//                            )
+//                        ).observe(this) {
+//                            if (it) {
+//                                getImageData(
+//                                    args.bean006.Jcxm,
+//                                    args.bean006.Ajywlb,
+//                                    args.bean006.Hjywlb,
+//                                    args.bean002.Ajlsh,
+//                                    args.bean002.Hjlsh
+//                                )
+//                                getSelectData(
+//                                    args.bean006.Jcxm,
+//                                    args.bean006.Ajywlb, args.bean006.Hjywlb,
+//                                    args.bean002.Ajlsh, args.bean002.Hjlsh
+//                                )
+//                            } else {
+//                                Toast.makeText(MyApp.context, "写入项目开始失败", Toast.LENGTH_SHORT).show()
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
 
 
     }
